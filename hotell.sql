40b2828605d94885e97d75840c6b0c89c3ea6760
Marija Gorbunova <mariarapirovna@gmail.com>
2024-04-16 09:38:40 +0300

    database room on loodud

--- hotell.sql
+++ hotell.sql
@@ -9,3 +9,21 @@ max_capacity int)
 select * from room_type;
 insert into room_type(description, max_capacity)
 values('super lux', 1);
+-- cntr s
+-- git add .
+-- git commit -a -m "tabel room_type on lisatud"
+-- gti push
+
+--tabel room
+create table room(
+id int primary key identity(1,1),
+number varchar(10),
+name varchar(40),
+status varchar(10),
+smoke bit,
+room_type_id int,
+foreign key (room_type_id) references room_type(id));
+select * from room;
+select * from room_type;
+insert into room (number, name, status, smoke, room_type_id)
+values ('E10', 'sv', 'open',1, 1);
