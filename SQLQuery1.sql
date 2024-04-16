ee1716c654214bf44cc44ab38d8c20e0f1cf62d8
Aleksandr Nadvornõi <nadvornyjaleksandr74@gmail.com>
2024-04-16 09:20:28 +0300

    tabeli room_type loomiseks

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,1 +1,14 @@
 create database HotelliReservNadvornoi
+use  HotelliReservNadvornoi
+--tabeli room_type
+create table room_type(
+id int identity(1,1),
+description_ varchar(80),
+max_capacity int); 
+
+select * from room_type;
+
+Insert into room_type(description_,max_capacity)
+Values('Super VIP LUX','5')
+Insert into room_type(description_,max_capacity)
+Values('Alone','1')
