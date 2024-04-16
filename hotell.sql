6e07367cef76726c548d6ba3c023edee5a254ccd
Lev Jegorov <Levjegorov22@gmail.com>
2024-04-16 09:37:12 +0300

    tabel room on lisatud

--- hotell.sql
+++ hotell.sql
@@ -11,3 +11,26 @@ SELECT *FROM room_type;
 INSERT INTO room_type(description, max_capacity)
 values ('Super lux',1);
 
+--git add .
+--git commit -a -m ""
+--git push
+
+
+--tabel room
+Create table room(
+id int primary key identity (1,1),
+number varchar(10),
+name varchar (40),
+status varchar(10),
+smoke bit,
+room_type_id int,
+foreign key (room_type_id) references room_type(id));
+SELECT * FROM room;
+SELECT * FROM room_type;
+
+INSERT INTO room(number,name,status,smoke,room_type_id)
+VALUES ('E10','sv','open',1,1);
+--git push
+--git add .
+--git commit -a -m ""
+--git push
