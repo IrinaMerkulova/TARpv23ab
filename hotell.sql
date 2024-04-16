e43d8b23538e61a8b653d3014fbf586f867bd889
Lev Jegorov <Levjegorov22@gmail.com>
2024-04-16 10:00:53 +0300

    tabel guest on lisatud

--- hotell.sql
+++ hotell.sql
@@ -35,5 +35,27 @@ VALUES ('E10','sv','open',1,1);
 --git commit -a -m ""
 --git push
 
+--tabel guest
+CREATE TABLE guest (
+id int primary key identity (1,1),
+first_name varchar(80),
+Last_name nvarchar(80),
+member_since date);
 
+INSERT INTO guest(first_name,Last_name,member_since)
+VALUES ('Lev','Jegorov','2007-06-28');
+--tabel reservation
+Create table reservation (
+id int primary key identity(1,1),
+date_in date,
+date_out date,
+made_by varchar(20),
+guest_id int,
+FOREIGN KEY(guest_id) references guest(id));
 
+select * from reservation
+
+INSERT INTO reservation(date_in,date_out,made_by,guest_id)
+VALUES('2020-05-24','2023-07-19','Lev Jegorov',1)
+
+create table reser
