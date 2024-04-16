a1c804c4c08aaeb5ed219f8d7a0349ba0cf4be7a
Martin_Sild <martinsild.mr@gmail.com>
2024-04-16 09:19:52 +0300

    tabel ruum_type on lisatud

--- hotell.sql
+++ hotell.sql
@@ -1,3 +1,16 @@
 -- Salvestame TARpv23ab kausta sisse CTRL + S
 create database hotellSild;
 use hotellSild;
+
+-- tabel room_type
+create table room_type(
+id int primary key identity(1,1),
+description varchar(80),
+max_capacity int);
+select * from room_type;
+insert into room_type(description, max_capacity)
+values('SUPER LUX',1)
+
+--drop table room_type;
+--delete from room_type;
+--where id = ...
