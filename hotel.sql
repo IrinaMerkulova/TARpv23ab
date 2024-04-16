649af3f64f05670040730113a6f22fae25db6b51
Daria Halchenko <daragalcenko3@gmail.com>
2024-04-16 09:37:20 +0300

    tabel room on lisatud

--- hotel.sql
+++ hotel.sql
@@ -13,3 +13,19 @@ Values (' super lux', 1);
 
 --DROP TABLE room_type;
 -- DELETE FROM room_type;
+
+
+-- tabel room
+CREATE TABLE room(
+id int Primary key identity (1,1),
+number varchar (10),
+name varchar (40),
+status varchar (10),
+smoke bit,
+room_type_id int,
+FOREIGN KEY (room_type_id) REFERENCES room_type(id));
+SELECT * FROM room;
+SELECT * FROM room_type;
+INSERT INTO room(number, name, status, smoke, room_type_id) 
+Values ('E10', 'sv', 'open', 1, 1);
+
