5936018fdf9c86efc1bbcec9dcb329ff916cd1f1
Marija Gorbunova <mariarapirovna@gmail.com>
2024-04-16 09:20:07 +0300

    tabel room_type on lisatud

--- hotell.sql
+++ hotell.sql
@@ -1,1 +1,11 @@
 --Salvestame TARpv23ab
+create database hotellGorbunova
+use hotellGorbunova
+-- tabel roomt_type
+create table room_type(
+id int primary key identity(1,1),
+description varchar(80),
+max_capacity int)
+select * from room_type;
+insert into room_type(description, max_capacity)
+values('super lux', 1);
