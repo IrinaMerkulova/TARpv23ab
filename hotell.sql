77a4444f2db434305c47490a11c46c2766c9d355
Bogdan Sergachev <kseeyz@gmail.com>
2024-04-16 09:37:02 +0300

    tabeli room on listatud

---
+++ hotell.sql
@@ -0,0 +1,29 @@
+--salvestame  TARpv23ab kausta sisse
+create database hotellSergachev;
+use hotellSergachev;
+
+create table room_type(
+id int Primary key identity(1,1),
+description varchar(80),
+max_capacity int);
+
+select * from room_type;
+
+insert into room_type( description, max_capacity)
+values('super lux', 1);
+
+-- tabel room
+create table room(
+id int Primary key identity(1,1),
+number varchar(10),
+name varchar(40),
+status varchar(10),
+smoke bit,
+room_type_id int,
+foreign key (room_type_id) references room_type(id));
+
+select *from room;
+
+insert into room(number, name, status, smoke, room_type_id)
+values('E10', 'sv', 'open', 1, 1);
+
