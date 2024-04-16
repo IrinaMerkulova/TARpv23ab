7c92cc94994b2c55d872a1ecda94f1746e54f9df
David Myrseth <david.mirsetSSS@gmail.com>
2024-04-16 09:37:09 +0300

    tabel room on lisatud

--- hotell.sql
+++ hotell.sql
@@ -13,3 +13,22 @@ VALUES ('super lux', 1);
 
 DROP TABLE  room_type;
 DELETE FROM room_type;
+
+--CTRL S
+--git add .
+--commit -a -m "tabel room_type on lisatud"
+--git push
+
+--tabel room
+CREATE TABLE room(
+id int Primary key identity (1,1),
+number varchar(10),
+name varchar(40),
+status varchar(40),
+smoke bit,
+room_type_id int,
+foreign key (room_type_id) references room_type(id));
+SELECT * FROM room;
+SELECT * FROM room_type;
+INSERT INTO room(number, name, status, smoke, room_type_id)
+VALUES ('E10', 'sv', 'open', 1, 1);
