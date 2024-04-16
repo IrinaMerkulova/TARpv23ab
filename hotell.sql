df92c6f5b80ef697f7f90eb2bfad555565f96280
Lev Jegorov <Levjegorov22@gmail.com>
2024-04-16 09:20:22 +0300

    tabel room_type on lisatud

--- hotell.sql
+++ hotell.sql
@@ -1,3 +1,13 @@
 --salvestame TARpv23ab kausta sisse ctrl+s
 CREATE database hotellJegorov;
-use hotellJegorov;
+use hotellJegorov;		
+
+--Tabel room_type
+CREATE TABLE room_type(
+id int PRIMARY KEY identity(1,1),
+description varchar(80),
+max_capacity int);
+SELECT *FROM room_type;
+INSERT INTO room_type(description, max_capacity)
+values ('Super lux',1);
+
