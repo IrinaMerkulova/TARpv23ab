fa85145991ff43f17f4723d074e10ff39bf632f4
Erik Gerega <erik.gerega@gmail.com>
2024-04-16 09:37:07 +0300

    tabel room on lisatud

--- Hotel.sql
+++ Hotel.sql
@@ -10,3 +10,18 @@ max_capacity int);
 select * from room_type;
 insert into room_type(description, max_capacity)
 values ('Super lux', 1);
+
+
+--tabel room
+create table room(
+id int primary key identity (1,1),
+number varchar(10),
+name varchar(40),
+status varchar(10),
+smoke bit,
+room_type_id int,
+foreign key (room_type_id) references room_type(id));
+select * from room_type
+select * from room
+insert into room(number, name, status, smoke, room_type_id)
+values('E10', 'sv', 'open', 1, 1)
