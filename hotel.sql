b6e2b8a9a470aed148b8d81298f14b55333e96d7
Jelizaveta Ostapjuk <jelizaveta.ostapjuk.work@gmail.com>
2024-04-16 09:19:49 +0300

    tabel room_type liisatud

--- hotel.sql
+++ hotel.sql
@@ -1,3 +1,12 @@
 --Salvestame TARpv23ab kausta sisse CTRL+S
 create database hotellOstapjuk;
 use hotellOstapjuk;
+
+--tabeli room_type
+create table room_type(
+id int PRIMARY KEY identity(1,1),
+description varchar(80),
+max_capacity int);
+select * from room_type
+insert into room_type(description,max_capacity)
+values('super lux',1);
