05f1debcda5af252316a03623643a5eba51c61ca
Aleksander Milishenko <doe481157@gmail.com>
2024-04-16 09:36:51 +0300

    room tabeli loomine

--- hotel.sql
+++ hotel.sql
@@ -11,3 +11,17 @@ SELECT * FROM room_type;
 INSERT INTO room_type(description ,max_capacity) VALUES('super lux', 2),('lux', 3);
 --DROP TABLE room_type;
 --DELETE FROM room_type;
+--CTRL + S
+--git add . / git commit -a -m "room_type loodud"
+
+--tabel room 
+CREATE TABLE room(
+id int primary key identity(1,1),
+number varchar(10),
+name varchar(40),
+status varchar(10),
+smoke bit,
+room_type_id int
+foreign key references room_type(id));
+
+insert into room(number, name, status, smoke,room_type_id) VALUES ('101','classroom','vaba',0,1);
