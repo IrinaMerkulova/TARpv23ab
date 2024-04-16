21c08d1e8f5401b7176a26f3db7eaabe54c94508
Vsevolod Tsarev <seva.tsarev@gmail.com>
2024-04-16 09:22:38 +0300

    tabel room_type on lisatud

--- hotell.sql
+++ hotell.sql
@@ -1,3 +1,12 @@
 --Salvestame TARpv23ab kausta sisse CTRL+S
 CREATE database hotellTsarev;
 use hotellTsarev;
+
+--tabel room_type
+CREATE TABLE room_type(
+id int Primary key identity(1,1),
+description varchar(80),
+max_capacity int);
+SELECT * FROM room_type;
+INSERT INTO room_type(description, max_capacity)
+VALUES('super lux', 1);
