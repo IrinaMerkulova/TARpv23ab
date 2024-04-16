c923797765dd0bdd7926fba88b7c3d5a00c95871
Vsevolod Tsarev <seva.tsarev@gmail.com>
2024-04-16 09:51:52 +0300

    tabel guest on lisatud

--- hotell.sql
+++ hotell.sql
@@ -30,5 +30,22 @@ INSERT INTO room(number, name, status, smoke, room_type_id)
 VALUES ('E10', 'sv', 'open', 1, 1);
 --CTRL + S
 --git add .
---git commit -a -m "tabel room_type on lisatud"
+--git commit -a -m "tabel room on lisatud"
 --git push
+--3. guest
+CREATE TABLE guest(
+id int Primary key identity(1,1),
+first_name varchar(80),
+last_name nvarchar(80),
+member_since date);
+SELECT * FROM guest;
+INSERT INTO guest(first_name, last_name, member_since)
+VALUES('Kirill', 'Sats', '2024-03-12');
+--CTRL + S
+--git add .
+--git commit -a -m "tabel guest on lisatud"
+--git push
+--4. reservation
+--5. reserved_room
+--6. occupied_room
+--7. hosted_at
