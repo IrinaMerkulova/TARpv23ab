25e2a11eb534c671d213babfbbddb9662659f8e1
Vladislav Kudriashev <fantomcorp@outlook.com>
2024-04-16 09:37:04 +0300

    --tabel room

--- hotell.sql
+++ hotell.sql
@@ -7,6 +7,20 @@ CREATE TABLE room_type(
 id int PRIMARY KEY identity(1,1),
 description varchar(80),
 max_capacity int);
-SELECT * FROM room_type
+SELECT * FROM room_type;
 INSERT INTO room_type(description,max_capacity)
 VALUES ('super ultra mega lux',1);
+
+--tabel room
+CREATE TABLE room(
+id int PRIMARY KEY identity(1,1),
+number varchar(10),
+name varchar(10),
+status varchar(10),
+smoke bit,
+room_type_id int,
+FOREIGN KEY (room_type_id) references room_type(id));
+SELECT * FROM room;
+SELECT * FROM room_type;
+INSERT INTO room(number,name,status,smoke,room_type_id)
+VALUES ('E10','sv','open',1,1);
