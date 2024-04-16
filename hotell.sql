d982b765fab134c7822bc32dc2f785de6575fd21
Vladislav Kudriashev <fantomcorp@outlook.com>
2024-04-16 09:19:24 +0300

    --tabel room_type

--- hotell.sql
+++ hotell.sql
@@ -1,3 +1,12 @@
 --Salvesame TARpv23ab kausta sisse CTRL+S
 create database hotellKudriashev;
 use hotellKudriashev;
+
+--tabel room_type
+CREATE TABLE room_type(
+id int PRIMARY KEY identity(1,1),
+description varchar(80),
+max_capacity int);
+SELECT * FROM room_type
+INSERT INTO room_type(description,max_capacity)
+VALUES ('super ultra mega lux',1);
