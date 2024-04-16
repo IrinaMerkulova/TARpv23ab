1b209af84d5aee074fa1b8af11096060670236a6
Martin_Sild <martinsild.mr@gmail.com>
2024-04-16 09:37:09 +0300

    tabel room on lisatud

--- hotell.sql
+++ hotell.sql
@@ -14,3 +14,20 @@ values('SUPER LUX',1)
 --drop table room_type;
 --delete from room_type;
 --where id = ...
+
+--tabel room
+
+create table room(
+id int primary key identity(1,1),
+number varchar(10),
+name varchar(40),
+status varchar(10),
+smoke bit,
+room_type_id int,
+FOREIGN KEY (room_type_id) references room_type(id));
+
+select * from room
+select * from room_type
+
+insert into room(number,name,status,smoke,room_type_id)
+values ('E10', 'sv', 'open', 1, 1);
