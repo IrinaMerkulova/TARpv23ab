dda5a2189112de8e1efc32235a4eca5b39c85b5f
Marija Gorbunova <mariarapirovna@gmail.com>
2024-04-16 09:59:14 +0300

    tabel reservation ja reserved_room on loodud

--- hotell.sql
+++ hotell.sql
@@ -34,4 +34,24 @@ first_name varchar(80),
 last_name varchar(80),
 member_since date)
 select * from guest
+
 -- luuan reservation
+create table reservation(
+id int primary key identity(1,1),
+date_in date,
+date_out date,
+made_by varchar(20),
+guest_id int,
+foreign key (guest_id) references guest(id));
+select * from reservation
+
+-- luuan tabel reserved_room 
+create table reserved_room(
+id int primary key identity(1,1),
+number_of_room int,
+room_type_id int 
+foreign key (room_type_id) references room_type(id),
+reservation_id int,
+foreign key (reservation_id) references reservation(id),
+status varchar(20));
+select * from reserved_room 
