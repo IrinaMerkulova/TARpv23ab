3c4a329d805ba13135ac0cb520af4e04ad0d1f33
Erik Gerega <erik.gerega@gmail.com>
2024-04-16 09:20:12 +0300

    tabel room_type on lisatud

--- Hotel.sql
+++ Hotel.sql
@@ -1,3 +1,12 @@
 --Salvestame TArpv23ab kausta sisse CTRL+S
 create database hotellGerega;
 use hotellGerega;
+
+--tabel room_type
+create table room_type(
+id int primary key identity (1,1),
+description varchar(80),
+max_capacity int);
+select * from room_type;
+insert into room_type(description, max_capacity)
+values ('Super lux', 1);
