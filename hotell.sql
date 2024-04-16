39a748cdb8fb4985630b1ce06a9ef878b6bb3295
David Myrseth <david.mirsetSSS@gmail.com>
2024-04-16 09:48:44 +0300

    tabel guest on lisatud

--- hotell.sql
+++ hotell.sql
@@ -16,7 +16,7 @@ DELETE FROM room_type;
 
 --CTRL S
 --git add .
---commit -a -m "tabel room_type on lisatud"
+--git commit -a -m "tabel room_type on lisatud"
 --git push
 
 --tabel room
@@ -32,3 +32,23 @@ SELECT * FROM room;
 SELECT * FROM room_type;
 INSERT INTO room(number, name, status, smoke, room_type_id)
 VALUES ('E10', 'sv', 'open', 1, 1);
+
+--CTRL S
+--git add .
+--git commit -a -m "tabel room_type on lisatud"
+--git push
+
+--tabeli guest
+CREATE TABLE guest(
+id int Primary key identity (1,1),
+first_name varchar(80),
+last_name varchar(80),
+member_since date);
+SELECT *FROM guest;
+insert into guest(first_name, last_name, member_since)
+VALUES ('David', 'Myrseth', '2024-04-16');
+
+--CTRL S
+--git add .
+--git commit -a -m "tabel room_type on lisatud"
+--git push
