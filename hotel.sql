f8ded53f75761232960026438b69c2f05efdaa59
Aleksander Milishenko <doe481157@gmail.com>
2024-04-16 09:19:59 +0300

    room_type loodud

--- hotel.sql
+++ hotel.sql
@@ -1,3 +1,13 @@
 --Salvestmine TARpv23ab kasuta sisse CTRL + S
 CREATE DATABASE hotellMilishenko; 
 use hotellMilishenko;
+
+-- tabel room_type
+CREATE TABLE room_type(
+id int Primary key identity(1,1),
+description varchar(80),
+max_capacity int);
+SELECT * FROM room_type;
+INSERT INTO room_type(description ,max_capacity) VALUES('super lux', 2),('lux', 3);
+--DROP TABLE room_type;
+--DELETE FROM room_type;
