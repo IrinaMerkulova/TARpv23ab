75c0cf65f14a75b8ddbe3dc52421e965d0234462
Aleksander Milishenko <doe481157@gmail.com>
2024-04-23 09:08:14 +0300

    lisamine andmed 'autod' tabelisse

--- autotabel.sql
+++ autotabel.sql
@@ -8,4 +8,4 @@ aasta int,
 regPiirk int
 );
 
-INSERT INTO 
+INSERT INTO autod(regNr, mark, aasta, regPiirk) VALUES ('123 ABC', 'Audi', 2000, 1 ), ('777 AAA', 'Ford', 1988, 2), ('FIN 772', 'Ford', 2002, 1), ('111 CCC', 'Nissan', 2006, 1), ('128 HGF', 'Toyota', 2003, 1), ('544 CCH','VAZ',1960,2);
