83a37996984c0cc9c2bcc7a524cd87dd37fd6f76
neflor1n <153904907+neflor1n@users.noreply.github.com>
2024-04-23 09:02:55 +0300

    tabel auto on loodud

--- autoTabel.sql
+++ autoTabel.sql
@@ -1,1 +1,15 @@
+-- SQL Server --
+
+create database autoSergachev;
+use autoSergachev;
+
+create table autod(
+  regNr char(7) primary key,
+  mark varchar(20),
+  aasta int,
+  regPiirkond int
+);
+  
+
+
 
