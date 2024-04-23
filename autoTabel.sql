b61bbc3d0dac5ed9e46740882e89ef62f7ec57e0
neflor1n <153904907+neflor1n@users.noreply.github.com>
2024-04-23 09:15:24 +0300

    Autod lisamine

--- autoTabel.sql
+++ autoTabel.sql
@@ -11,5 +11,11 @@ create table autod(
 );
   
 
-
+insert autod(regNr, mark, aasta, regPiirkond)
+values('123 ABC', 'Audi', 2000, 1),
+('777 AAA', 'Ford', 1988, 2),
+('FIN 772', 'Ford', 2002, 1),
+('111 CCC', 'Nissan', 2006, 1),
+('128 HGF', 'Toyota', 2003, 1),
+('544 CCH', 'VAZ', 1960, 2);
 
