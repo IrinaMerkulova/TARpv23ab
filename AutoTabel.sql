cf4a191943bd8ada811f1e45cbaf58364acdc781
Lev Jegorov <153904776+Levonin@users.noreply.github.com>
2024-04-23 09:24:39 +0300

    tegime insert into tabelisse

--- AutoTabel.sql
+++ AutoTabel.sql
@@ -3,6 +3,16 @@ CREATE DATABASE AutoJegorov;
 USE AutoJegorov;
 CREATE TABLE Autod (
   regNr char(7) primary key,
-  mark varchar(20) ,
+  mark varchar(20) A
   aasta int,
-  regPiirkond int)
+  regPiirkond int,
+  );
+
+INSERT INTO Autod (regNr, mark, aasta, regPiirkond)
+VALUES
+('123 ABC', 'Audi', 2000, 1),
+('777 AAA', 'Ford', 1988, 2),
+('FIN 772', 'Ford', 2002, 1),
+('111 CCC', 'Nissan', 2006, 1),
+('128 HGF', 'Toyota', 2003, 1),
+('544 CCH', 'VAZ', 1960, 2);
