fdb719630aaa1a96d06dccb687b1302dff08a1cb
Aleksandra-Semjonova <153904971+Aleksandra-Semjonova@users.noreply.github.com>
2024-04-23 09:53:10 +0300

    Sisestatud on järgmised 5 autot

--- autoTabel.sql
+++ autoTabel.sql
@@ -1,1 +1,24 @@
+--sql Server
+CREATE DATABASE autoSemjonova;
+USE autoSemjonova;
+CREATE TABEL autod(
+  regNr char(7) Primary Key,
+  mark varchar(20),
+  aasta int, 
+  regPiirkond int)
+  
+SELECT * FROM autod
+INSERT INTO autod(regNr, mark, aasta, regPiirkond)
+Values ('123 ABC', 'Audi', 2000, 1);
+INSERT INTO autod(regNr, mark, aasta, regPiirkond)
+Values ('777 AAA', 'Ford', 1988, 2);
+INSERT INTO autod(regNr, mark, aasta, regPiirkond)
+Values ('FIN 772', 'Ford', 2002, 1);
+INSERT INTO autod(regNr, mark, aasta, regPiirkond)
+Values ('111 CCC', 'Nissan', 2006, 1);
+INSERT INTO autod(regNr, mark, aasta, regPiirkond)
+Values ('128 HGF', 'Toyota', 2003, 1);
+INSERT INTO autod(regNr, mark, aasta, regPiirkond)
+Values ('544 CCH', 'VAZ', 1960, 2);
 
+  
