3949ad36950b47700891ae8e8b23f1520c1ad314
kirillsats <153904960+kirillsats@users.noreply.github.com>
2024-04-23 10:01:54 +0300

    Lisasin omad 20 autod

--- autoTabel.sql
+++ autoTabel.sql
@@ -6,7 +6,7 @@ CREATE table autod(
   mark varchar(20),
   aasta int,
   regPiirkond int)
-SELECT * FROM autod
+
 INSERT INTO autod (regNr, mark, aasta, regPiirkond)
 VALUES ('123 ABC', 'Audi', 2000, 1) 
 VALUES ('777 AAA', 'Ford', 1998, 2) 
@@ -14,5 +14,25 @@ VALUES ('FIN 772', 'Ford', 2002, 1)
 VALUES ('111 CCC', 'Nissan', 2006, 1) 
 VALUES ('128 HGF', 'Toyota', 2003, 1) 
 VALUES ('544 CCH', 'VAZ', 1960, 2) 
-
+SELECT * FROM autod
+insert into autod (regNr, mark, aasta, regPiirkond) values ('007 KIR', 'BMW',  1997, 4);
+insert into autod (regNr, mark, aasta, regPiirkond) values ('111 KOL', 'Mazda', 2005, 2);
+insert into autod (regNr, mark, aasta, regPiirkond) values ('222 TAK', 'Lexus', 1997, 71);
+insert into autod (regNr, mark, aasta, regPiirkond) values ('123 NOM', 'Volkswagen', 1998, 8);
+insert into autod (regNr, mark, aasta, regPiirkond) values ('121 JEP', 'Mercury', 1999, 77);
+insert into autod (regNr, mark, aasta, regPiirkond) values ('456 PON', 'Ford', 1996, 92);
+insert into autod (regNr, mark, aasta, regPiirkond) values ('001 JAA','Ford', 2000, 54);
+insert into autod (regNr, mark, aasta, regPiirkond) values ('DE1MARK','Chevrolet', 1978, 44);
+insert into autod (regNr, mark, aasta, regPiirkond) values ('666 BUZ', 'Buick', 2005, 57);
+insert into autod (regNr, mark, aasta, regPiirkond) values ('777 TOP', 'Subaru',  2009, 44);
+insert into autod (regNr, mark, aasta, regPiirkond) values ('555 BOG', 'Chrysler', 1997, 88);
+insert into autod (regNr, mark, aasta, regPiirkond) values ('112 KOP', 'Mercury', 1993, 57);
+insert into autod (regNr, mark, aasta, regPiirkond) values ('654 AUI', 'Ford', 1955, 34);
+insert into autod (regNr, mark, aasta, regPiirkond) values ('654 RZF', 'Pontiac', 1962, 36);
+insert into autod (regNr, mark, aasta, regPiirkond) values ('878 BCB', 'Chevrolet', 2001, 4);
+insert into autod (regNr, mark, aasta, regPiirkond) values ('327 MRN', 'Chrysler', 2000, 96);
+insert into autod (regNr, mark, aasta, regPiirkond) values ('312 KKK', 'Mazda', 2001, 27);
+insert into autod (regNr, mark, aasta, regPiirkond) values ('333 DDD', 'Mazda', 1993, 63);
+insert into autod (regNr, mark, aasta, regPiirkond) values ('122 POL', 'Jaguar', 2007, 79);
+insert into autod (regNr, mark, aasta, regPiirkond) values('111 PFF', 'Chevrolet', 1996, 46);
 
