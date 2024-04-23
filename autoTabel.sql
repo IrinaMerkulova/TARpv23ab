3049316a56c852c450b91a392a94dad0abf03f75
kirillsats <153904960+kirillsats@users.noreply.github.com>
2024-04-23 09:45:33 +0300

    Update autoTabel.sql

--- autoTabel.sql
+++ autoTabel.sql
@@ -6,3 +6,13 @@ CREATE table autod(
   mark varchar(20),
   aasta int,
   regPiirkond int)
+SELECT * FROM autod
+INSERT INTO autod (regNr, mark, aasta, regPiirkond)
+VALUES ('123 ABC', 'Audi', 2000, 1) 
+VALUES ('777 AAA', 'Ford', 1998, 2) 
+VALUES ('FIN 772', 'Ford', 2002, 1) 
+VALUES ('111 CCC', 'Nissan', 2006, 1) 
+VALUES ('128 HGF', 'Toyota', 2003, 1) 
+VALUES ('544 CCH', 'VAZ', 1960, 2) 
+
+
