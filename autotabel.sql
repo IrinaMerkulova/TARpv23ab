42a28b8161e06fa2bab35a683ec777a3b9e2ac2c
Kirilltsyogankov <154509304+Kirilltsyogankov@users.noreply.github.com>
2024-04-23 09:02:32 +0300

    tabel auto on loodud

--- autotabel.sql
+++ autotabel.sql
@@ -1,1 +1,9 @@
-
+--sql server
+create database autoTsogankov;
+use autoTsogankov;
+CREATE TABLE Autod (
+    RegNr VARCHAR(7) PRIMARY KEY,
+    Mark VARCHAR(20),
+    Aasta INT,
+    RegPiirk INT
+);
