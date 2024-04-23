bbcb6eab08f36290512f37c2cd3078a753c04216
DariaHalchenko <154507353+DariaHalchenko@users.noreply.github.com>
2024-04-23 09:11:29 +0300

    tabeli täitmine

--- Autotabel.sql
+++ Autotabel.sql
@@ -5,4 +5,12 @@ CREATE table autod(
   regNr char(7) Primary key,
   mark varchar(20),
   aasta int,
-  regPiirkond int) 
+  regPiirkond int);
+INSERT INTO autod(regNr, mark, aasta, regPiirkond) 
+Values ('123 ABC' , 'Audi', 2000, 1),
+('777 AAA' , 'Ford', 1988, 2),
+('FIN 772' , 'Ford', 2002, 1),
+('111 CCC' , 'Nissan', 2006, 1),
+('128 HGF' , 'Toyota', 2003, 1),
+('544 CCH' , 'VAZ', 1960, 2);
+Select * from autod;
