04fca15e8b03985901bb8f5071b062e9827ee5cb
BogdanViblyy <153904857+BogdanViblyy@users.noreply.github.com>
2024-04-23 09:17:18 +0300

    Update autotabel.sql
    sissetasin esimesed autod tabelisse

--- autotabel.sql
+++ autotabel.sql
@@ -9,3 +9,7 @@ CREATE TABLE autod(
 
 INSERT autod VALUES ('Audi', '123 ABC', 2000, 1)
 INSERT autod VALUES ('Ford', '777 AAA', 1988, 2)
+INSERT autod VALUES ('Ford', 'FIN 772', 2002, 1)
+INSERT autod VALUES ('Nissan', '111 CCC', 2006, 1)
+INSERT autod VALUES ('Toyota', '128 HGF', 2003, 1)
+INSERT autod VALUES ('VAZ', '544 CCH', 1960, 2)
