bbdc4eb3758fa71a1b5a314abb52ed42a18693f7
Copper <82279357+synd4772@users.noreply.github.com>
2024-04-23 09:01:41 +0300

    tabel auto on loodud

--- autotabel.sql
+++ autotabel.sql
@@ -1,1 +1,11 @@
+-- SQL Server
+CREATE DATABASE autoMilishenko;
+USE autoMilishenko;
+CREATE TABLE autod(
+regNr char(7) primary key,
+mark varchar(255),
+aasta int,
+regPiirk int
+);
 
+INSERT INTO 
