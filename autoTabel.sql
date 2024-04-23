571010b43a883df434c453a2b67ccd8c65f33ff0
Timur-Basirov <153905042+Timur-Basirov@users.noreply.github.com>
2024-04-23 09:31:25 +0300

    autoTabel.sql

--- autoTabel.sql
+++ autoTabel.sql
@@ -7,3 +7,11 @@ Mark varchar(20),
 Aasta int,
 RegPiirk int
 );
+
+INSERT INTO autod (regNr,Mark,Aasta,RegPiirk) 
+VALUES ('123 ABC', 'Audi', 2000, 1),
+('777 AAA', 'Ford', 1988, 2),
+('FIN 772', 'Ford', 2002, 1),
+('111 CCC', 'Nissan', 2006, 1),
+('128 HGF', 'Toyota', 2003, 1),
+('544 CCH', 'VAZ', 1960, 2);
