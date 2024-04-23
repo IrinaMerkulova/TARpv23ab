8b76c4444e7e0793f60adb74dcb7442c744f4da9
FantomTchi7 <fantomcorp@outlook.com>
2024-04-23 09:21:52 +0300

    Values on loodud

--- autoTabel.sql
+++ autoTabel.sql
@@ -8,3 +8,14 @@ Mark varchar(20),
 Aasta int,
 RegPiirk int
 )
+
+insert into auto(Mark, RegNr, Aasta, RegPiirk)
+values
+('Audi','123 ABC',2000,1),
+('Ford','777 AAA',1988,2),
+('Ford','FIN 772',2002,1),
+('Nissan','111 CCC',2006,1),
+('Toyota','128 HGF',2003,1),
+('VAZ','544 CCH',1960,2);
+
+select * from auto;
