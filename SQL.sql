8dc7698c42dc05146271cd54411ef872b5a00488
MarijaGorbunova0 <153904965+MarijaGorbunova0@users.noreply.github.com>
2024-04-24 15:00:24 +0300

    vastused SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -7,52 +7,52 @@ table_name - Название таблицы
 
 CREATE TABLE (column_name1 datatype, ...)
 -----------------
-CREATE TABLE - 
-table_name - 
-(column_name1 datatype, ...) - 
+CREATE TABLE - создание таблицы
+table_name - имя таблицы
+(column_name1 datatype, ...) - столбики в таблице
 
-INSERT INTO table_name(column_name1, ...)
-VALUES (column_value1, ...)
+INSERT INTO table_name(column_name1, ...) 
+VALUES (column_value1, ...) 
 -----------------
-INSERT INTO - 
-table_name - 
-(column_name1 datatype, ...) - 
-VALUES (column_value1, ...) - 
+INSERT INTO -  - добавить данные в таблицу
+table_name - имя таблицы
+(column_name1 datatype, ...) - в какие столбики
+VALUES (column_value1, ...) - какие даннык будут 
 
-DROP TABLE table_name
+DROP TABLE table_name 
 -----------------
-DROP TABLE - 
-table_name - 
+DROP TABLE -  Удалить таблицу
+table_name - имя таблицы
 
 ------------------------------------
 
 UPDATE table_name SET column_name1=column_value1
 WHERE condition
 -----------------
-UPDATE - 
-table_name - 
-SET - 
-column_name1=column_value1 - 
-WHERE - 
-condition - 
+UPDATE - обновить
+table_name - имя таблицы
+SET - Установить 
+column_name1=column_value1 - где одно равно другому
+WHERE - где
+condition - условие
 
 ALTER TABLE table_name ADD column_name datatype
 -----------------
-ALTER TABLE - 
-table_name - 
-ADD - 
-column_name2 datatype - 
+ALTER TABLE - Изменить таблицу 
+table_name - имя таблицы
+ADD - добавить 
+column_name2 datatype - тип Данных
 
 -------------------------------
 
 ALTER TABLE table_name 
 ADD FOREIGN KEY (column_name) References table_name2(column_name2)
 -----------------
-ALTER TABLE -
-table_name - 
-ADD - 
-FOREIGN KEY - 
-(column_name) - 
-References - 
-table_name2 - 
-(column_name2) - 
+ALTER TABLE - Изменить таблицу
+table_name - имя таблицы
+ADD - добавить
+FOREIGN KEY - внешний ключ 
+(column_name) - имя столбика
+References - определения внешних ключей 
+table_name2 - имя таблицы
+(column_name2) - имя столбика 2
