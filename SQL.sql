77f77e2b5d53be651c637c3f8d027348801acb4a
neflor1n <153904907+neflor1n@users.noreply.github.com>
2024-04-24 14:46:53 +0300

    mis on "insert into" sql'is

--- SQL.sql
+++ SQL.sql
@@ -7,16 +7,16 @@ table_name - Название таблицы
 
 CREATE TABLE (column_name1 datatype, ...)
 -----------------
-CREATE TABLE - 
-table_name - 
-(column_name1 datatype, ...) - 
+CREATE TABLE - создать таблицу
+table_name - Название таблицы
+(column_name1 datatype, ...) - column_name1 - название столбца datatype - тип данных
 
 INSERT INTO table_name(column_name1, ...)
 VALUES (column_value1, ...)
 -----------------
 INSERT INTO - 
 table_name - 
-(column_name1 datatype, ...) - 
+(column_name1 datatype, ...) -  
 VALUES (column_value1, ...) - 
 
 DROP TABLE table_name
