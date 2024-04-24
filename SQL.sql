c62a53791b707bed1feec5d1f957173c4cbc19e1
kirillsats <153904960+kirillsats@users.noreply.github.com>
2024-04-24 14:53:37 +0300

    mis on 'DROP TABLE'

--- SQL.sql
+++ SQL.sql
@@ -21,8 +21,8 @@ VALUES (column_value1, ...) - список значений
 
 DROP TABLE table_name
 -----------------
-DROP TABLE - 
-table_name - 
+DROP TABLE - удалить таблицу
+table_name - название таблицы
 
 ------------------------------------
 
