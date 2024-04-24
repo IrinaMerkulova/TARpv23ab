c60e8d1ee11e12a09c2e7c3504755e54e9af23df
Martin Sild <martinsild.mr@gmail.com>
2024-04-24 14:57:30 +0300

    SQL.sql on taidetud, w3Quizz.txt on tehtud ylesanne ja kirjutatud oiget vastused kus oli viga

--- SQL.sql
+++ SQL.sql
@@ -7,52 +7,52 @@ table_name - Название таблицы
 
 CREATE TABLE (column_name1 datatype, ...)
 -----------------
-CREATE TABLE - 
-table_name - 
+CREATE TABLE - создать таблицу
+table_name - название таблицы
 (column_name1 datatype, ...) - 
 
 INSERT INTO table_name(column_name1, ...)
 VALUES (column_value1, ...)
 -----------------
-INSERT INTO - 
-table_name - 
+INSERT INTO - вставка новых запесей
+table_name - название таблицы
 (column_name1 datatype, ...) - 
 VALUES (column_value1, ...) - 
 
 DROP TABLE table_name
 -----------------
-DROP TABLE - 
-table_name - 
+DROP TABLE - удалить таблицу
+table_name - название таблицы
 
 ------------------------------------
 
 UPDATE table_name SET column_name1=column_value1
 WHERE condition
 -----------------
-UPDATE - 
-table_name - 
-SET - 
+UPDATE - обновить
+table_name - название таблицы
+SET - какие имменно
 column_name1=column_value1 - 
-WHERE - 
-condition - 
+WHERE - фильтрации строк данных на основе заданных условий
+condition - логическое вырожение
 
 ALTER TABLE table_name ADD column_name datatype
 -----------------
-ALTER TABLE - 
-table_name - 
-ADD - 
-column_name2 datatype - 
+ALTER TABLE - изменение структуры
+table_name - название таблицы
+ADD - ограничений к существующей таблице
+column_name2 datatype - определения нового столбца в таблице
 
 -------------------------------
 
 ALTER TABLE table_name 
 ADD FOREIGN KEY (column_name) References table_name2(column_name2)
 -----------------
-ALTER TABLE -
-table_name - 
-ADD - 
-FOREIGN KEY - 
-(column_name) - 
-References - 
-table_name2 - 
-(column_name2) - 
+ALTER TABLE - изменение структуры
+table_name - название таблицы
+ADD - ограничений к существующей таблице
+FOREIGN KEY - вторичный ключ
+(column_name) - какие столбцы в таблице будут связаны внешним ключом
+References - указывающее на таблицу и столбец, на которые будет ссылаться вторичный ключ
+table_name2 - назание таблицы
+(column_name2) - какие столбцы в таблице будут связаны внешним ключом 
