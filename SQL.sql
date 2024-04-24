1b8806dc45004b20cdcf36948c8d69bf621eb738
Aleksandra-Semjonova <153904971+Aleksandra-Semjonova@users.noreply.github.com>
2024-04-24 14:57:47 +0300

    Update SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -7,22 +7,22 @@ table_name - Название таблицы
 
 CREATE TABLE (column_name1 datatype, ...)
 -----------------
-CREATE TABLE - 
-table_name - 
+CREATE TABLE - создать таблицу
+table_name - название таблицы
 (column_name1 datatype, ...) - 
 
 INSERT INTO table_name(column_name1, ...)
-VALUES (column_value1, ...)
+VALUES (column_value1, ...) 
 -----------------
-INSERT INTO - 
-table_name - 
+INSERT INTO - добавляет запись в таблицу
+table_name - название таблицы
 (column_name1 datatype, ...) - 
-VALUES (column_value1, ...) - 
+VALUES (column_value1, ...) - вычисляет строковое значение 
 
 DROP TABLE table_name
 -----------------
-DROP TABLE - 
-table_name - 
+DROP TABLE -  удалить таблицу 
+table_name - название таблицы
 
 ------------------------------------
 
@@ -30,29 +30,29 @@ UPDATE table_name SET column_name1=column_value1
 WHERE condition
 -----------------
 UPDATE - 
-table_name - 
-SET - 
-column_name1=column_value1 - 
-WHERE - 
-condition - 
+table_name - название таблицы
+SET - создать значение 
+column_name1=column_value1 - название столбца1 равноется названию столбца2
+WHERE - где
+condition - условие 
 
 ALTER TABLE table_name ADD column_name datatype
 -----------------
-ALTER TABLE - 
-table_name - 
-ADD - 
-column_name2 datatype - 
+ALTER TABLE - изменияет тип таблица 
+table_name - название таблицы
+ADD - добавляет 
+column_name2 datatype - название столбца 2  тип данных
 
 -------------------------------
 
 ALTER TABLE table_name 
 ADD FOREIGN KEY (column_name) References table_name2(column_name2)
 -----------------
-ALTER TABLE -
-table_name - 
-ADD - 
-FOREIGN KEY - 
-(column_name) - 
+ALTER TABLE -изменияет тип таблица 
+table_name - название таблицы
+ADD - добавляет 
+FOREIGN KEY - вторичный ключ
+(column_name) - название столбца
 References - 
-table_name2 - 
-(column_name2) - 
+table_name2 - название таблицы 2
+(column_name2) - название столбца 2
