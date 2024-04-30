5988c72ec268bc13caa6a93484f5c9b9e7b26fe4
DavidMyrseth <153905309+DavidMyrseth@users.noreply.github.com>
2024-04-30 11:46:19 +0300

    -- See päring valib tabelist "Person" kõige vanema inimese vanuse.

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -195,7 +195,8 @@ select min(cast(Age as int)) from Person
 
 --- kõige vanem isik
 select max(cast(Age as int)) from Person
-
+-- See päring valib tabelist "Person" kõige vanema inimese vanuse.
+	
 select City, sum(cast(Age as int)) as TotalAge from Person group by City
 
 
