35e251661d34657fdeee03630a0e1adc6451a8e3
Martin Sild <martinsild.mr@gmail.com>
2024-04-30 10:11:29 +0300

    luua salvestatud protseduur, saada töötajate arv soo järgi

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -295,7 +295,7 @@ spGetEmployeesByGenderAndDepartment @DepartmentId =  1, @Gender = 'Male'
 
 
 
---?
+-- luua salvestatud protseduur, saada töötajate arv soo järgi
 create proc spGetEmployeeCountByGender
 @Gender nvarchar(20),
 @EmployeeCount int output
