6901795427d2db50266f062ee02fa9220acc13f3
Martin Kemppi <dusperin1234@gmail.com>
2024-04-30 10:17:26 +0300

    kutsuda

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -347,7 +347,7 @@ as begin
 	return (select FirstName from Employees where Id = @Id)
 end
 
--- ?
+-- kutsuda spGetNameById2-protseduuri töötaja ID 1 ja salvestada tulemus muutujasse @EmployeeName.
 declare @EmployeeName nvarchar(50)
 exec @EmployeeName = spGetNameById2 1
 print 'Name of the employee = ' + @EmployeeName
