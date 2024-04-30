915ad328c672dfe7c248b5489c3c3eebfab68743
FantomTchi7 <fantomcorp@outlook.com>
2024-04-30 09:08:03 +0300

    --eemalda tabelist isik ID-ga 8

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -85,7 +85,7 @@ add constraint CK_Person_Age check (Age > 0 and Age < 150)
 insert into Person (Id, Name, Email, GenderId, Age)
 values (9, 'Test', 'Test', 2, 149)
 
---?
+--eemalda tabelist isik ID-ga 8 
 select * from Person
 go
 delete from Person where Id = 8
