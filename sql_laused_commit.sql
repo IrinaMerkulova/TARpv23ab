d4c59a73b6a8b076e2fd68c099cb817940f26f1f
FantomTchi7 <fantomcorp@outlook.com>
2024-04-30 09:21:39 +0300

    -- Näitab töötajate nimid ja osakonna Id'ed

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -209,7 +209,7 @@ values (10, 'Russell', 'Male', 8800, NULL)
 
 select * from Employees
 
----?
+-- Näitab töötajate nimid ja osakonna Id'ed
 select distinct Name, DepartmentId from Employees
 
 ---?
