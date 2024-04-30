8e27929578aa242a31e4b9768137abd0144bb553
DavidMyrseth <153905309+DavidMyrseth@users.noreply.github.com>
2024-04-30 11:19:56 +0300

    Update sql_laused_commit.sql

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -133,7 +133,8 @@ select * from Person where City = 'Gotham'
 -- kõik, kes ei ela Gothamis
 select * from Person where City <> 'Gotham'
 select * from Person where City != 'Gotham'
-
+-- Valib kõik isikud, kes ei ela linnas Gotham.
+	
 -- näitab teatud vanusega inimesi
 select *from Person where Age = 100 or 
 Age = 50 or Age = 20
