83e658a2cec9cfc74460c43cc691e7b2b5088764
Bogdan Sergachev <kseeyz@gmail.com>
2024-04-30 09:12:30 +0300

    Kustutab kasutaja ID 8 all olevas tabelis Isik

--- sql_laused_commit – koopia.sql
+++ sql_laused_commit – koopia.sql
@@ -86,7 +86,7 @@ add constraint CK_Person_Age check (Age > 0 and Age < 150)
 insert into Person (Id, Name, Email, GenderId, Age)
 values (9, 'Test', 'Test', 2, 160)
 
--- 
+-- Удаляет пользователя в таблице Person под айди 8
 
 select * from Person
 go
