1ac3ec214ba05ed7828c0bba2f2dd7eca5602c1c
Aleksandra-Semjonova <aleksandra.semjonova24@gmail.com>
2024-04-30 10:13:49 +0300

    --- loob protseduuri, kus saab teada, kui palju inimesi ettevõttes töötab

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -315,7 +315,7 @@ declare @TotalCount int
 exec spGetEmployeeCountByGender @EmployeeCount = @TotalCount out, @Gender = 'Male'
 print @TotalCount
 
----?
+--- loob protseduuri, kus saab teada, kui palju inimesi ettevõttes töötab
 create proc spTotalCount2
 @TotalCount int output
 as begin
