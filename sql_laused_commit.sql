b634b3bf678e94c69678e309ac0ac943a38d41f9
Martin Kemppi <dusperin1234@gmail.com>
2024-04-30 09:54:20 +0300

    Sorteerib kirjed

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -137,7 +137,7 @@ select top 3 Age, Name from Person
 
 --- näitab esimesed 50% tabelis
 select top 50 percent * from Person
---?
+-- Sorteerib kirjed selle täisarvulise väärtuse järgi
 select * from Person order by cast(Age as int)
 select * from Person order by Age
 
