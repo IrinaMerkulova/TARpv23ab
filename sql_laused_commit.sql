c520355c44585fe22a15cb30bddccca3798fe9d9
Artur Shushkevitch <artursuskevits@gmail.com>
2024-04-30 11:32:54 +0300

    ma ei tea kuidas seda selgitada

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -334,7 +334,7 @@ as begin
 	select @FirstName = FirstName from employees where Id = @Id
 end
 
---?
+--ma ei tea kuidas seda selgitada
 declare @FirstName nvarchar(50)
 execute spGetNameById1 6, @FirstName output
 print 'Name of the employee = ' + @FirstName
