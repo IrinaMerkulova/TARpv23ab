884dc34ccb1470dce0f436f8c51192a890eb0fb1
Opilane TTHK <opilane@tthk.ee>
2024-04-30 09:30:43 +0300

    andmete Female ja Male  sisestamine tabelisse

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -19,7 +19,7 @@ Email nvarchar(30),
 GenderId int
 )
 
---- andmete Person ja Gender  sisestamine tabelisse 
+--- andmete Female ja Male  sisestamine tabelisse 
 insert into Gender (Id, Gender)
 values (1, 'Female')
 insert into Gender (Id, Gender)
