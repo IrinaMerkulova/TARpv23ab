2f0bc8f4c6f6e6e10f563ffe302045d36f77393c
DavidMyrseth <153905309+DavidMyrseth@users.noreply.github.com>
2024-04-30 11:20:33 +0300

    Update sql_laused_commit.sql

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -139,7 +139,8 @@ select * from Person where City != 'Gotham'
 select *from Person where Age = 100 or 
 Age = 50 or Age = 20
 select * from Person where Age in (100, 50, 20)
-
+-- Valib kõik isikud, kelle vanus on 100, 50 või 20 aastat.
+	
 --- näitab teatud vanusevahemikus olevaid inimesi
 select * from Person where Age between 30 and 50
 
