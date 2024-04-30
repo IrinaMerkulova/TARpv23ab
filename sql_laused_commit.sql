38a761beae125d35ca97dc6f5c0e2675158286d1
FantomTchi7 <fantomcorp@outlook.com>
2024-04-30 09:37:34 +0300

    -- teostab protseduuri, saab nime ID järgi

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -336,7 +336,7 @@ as begin
 	select @FirstName = FirstName from employees where Id = @Id
 end
 
---?
+-- teostab protseduuri, saab nime ID järgi
 declare @FirstName nvarchar(50)
 execute spGetNameById1 6, @FirstName output
 print 'Name of the employee = ' + @FirstName
