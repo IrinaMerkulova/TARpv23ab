42706317c2769af779a56f602eab37a93f4d8c61
Artur Shushkevitch <artursuskevits@gmail.com>
2024-04-30 11:33:50 +0300

    näitab töötaja nimi kellel on valitud id

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -339,7 +339,7 @@ declare @FirstName nvarchar(50)
 execute spGetNameById1 6, @FirstName output
 print 'Name of the employee = ' + @FirstName
 
---?
+--näitab töötaja nimi kellel on valitud id
 create proc spGetNameById2
 @Id int
 as begin
