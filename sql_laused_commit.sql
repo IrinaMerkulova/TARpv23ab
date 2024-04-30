a80de315d339df57e8c6528e0912546c48fe4cf0
Martin Kemppi <dusperin1234@gmail.com>
2024-04-30 10:13:22 +0300

    tagastab

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -335,7 +335,7 @@ as begin
 	select @FirstName = FirstName from employees where Id = @Id
 end
 
---?
+-- tagastab töötaja nime selle identifikaatori (6) järgi ja kuvab selle ekraanil.
 declare @FirstName nvarchar(50)
 execute spGetNameById1 6, @FirstName output
 print 'Name of the employee = ' + @FirstName
