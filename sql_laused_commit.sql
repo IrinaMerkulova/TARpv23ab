7e1c21af8b1f26a534dac956be69b532e429995b
Lev Jegorov <Levjegorov22@gmail.com>
2024-04-30 09:24:34 +0300

    päring lisab Personi tabelisse välise võtme nimega

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -25,7 +25,7 @@ values (1, 'Female')
 insert into Gender (Id, Gender)
 values (2, 'Male')
 
---- ?
+--- päring lisab Personi tabelisse välise võtme nimega 
 alter table Person add constraint tblPerson_GenderId_FK
 foreign key (GenderId) references Gender(Id)
 
