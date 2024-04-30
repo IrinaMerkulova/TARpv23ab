f69fe6fea962c116bf8e422c349ab38b798337c2
Gleb Sotsjov <opilane@tthk.ee>
2024-04-30 10:05:34 +0300

    tagastab töötaja nime antud id järgi

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -348,7 +348,7 @@ declare @FirstName nvarchar(50)
 execute spGetNameById1 6, @FirstName output
 print 'Name of the employee = ' + @FirstName
 
---?
+-- tagastab töötaja nime antud id järgi
 create proc spGetNameById2
 @Id int
 as begin
