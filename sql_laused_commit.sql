138e693f38586543ddb1c424cc04c95e63197520
Aleksander Milishenko <doe481157@gmail.com>
2024-04-30 09:21:24 +0300

    kaheksateistkumnes

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -326,7 +326,7 @@ declare @TotalEmployees int
 execute spTotalCount2 @TotalEmployees output
 select @TotalEmployees
 
---- ?
+--- loob protseduuri, mis näitab 
 create proc spGetNameById1
 @Id int,
 @FirstName nvarchar(50) output
