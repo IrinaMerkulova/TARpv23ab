3f2c98462d4f6522d955f2aa814adf40a3a9656d
Jelizaveta Ostapjuk <jelizaveta.ostapjuk.work@gmail.com>
2024-04-30 09:32:22 +0300

    näitab ainult üks kord nime, kui see kordub

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -209,7 +209,7 @@ values (10, 'Russell', 'Male', 8800, NULL)
 
 select * from Employees
 
----?
+--- näitab ainult üks kord nime, kui see kordub
 select distinct Name, DepartmentId from Employees
 
 ---?
