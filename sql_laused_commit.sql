6a3090bc798c0302097f349ff516744b13554f41
Marija Gorbunova <mariarapirovna@gmail.com>
2024-04-30 11:29:35 +0300

    protseduur mis saab nime ID järgi

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -330,7 +330,7 @@ as begin
 	select @FirstName = FirstName from employees where Id = @Id
 end
 
---?
+-- teeb protseduuri mis saab nime ID järgi
 declare @FirstName nvarchar(50)
 execute spGetNameById1 6, @FirstName output
 print 'Name of the employee = ' + @FirstName
