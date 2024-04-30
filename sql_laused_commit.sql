98481e6c472541d487ee72e471043a3e3439cd13
Opilane TTHK <opilane@tthk.ee>
2024-04-30 09:38:05 +0300

    sisestame väärtuse tabelisse Gender

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -52,7 +52,7 @@ select * from Person
 alter table Person
 drop constraint tblPerson_GenderId_FK
 
--- sisestame väärtuse tabelisse
+-- sisestame väärtuse tabelisse Gender 
 insert into Gender (Id, Gender)
 values (3, 'Unknown')
 -- lisame võõrvõtme uuesti
