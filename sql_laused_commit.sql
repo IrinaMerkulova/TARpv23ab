c26f393a63677dc5185e427d10c6a0ce9f2a7651
DavidMyrseth <153905309+DavidMyrseth@users.noreply.github.com>
2024-04-30 11:17:35 +0300

    Update sql_laused_commit.sql

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -110,7 +110,8 @@ add constraint CK_Person_Age check (Age > 0 and Age < 150)
 
 insert into Person (Id, Name, Email, GenderId, Age)
 values (9, 'Test', 'Test', 2, 160)
-
+-- Üritab sisestada kirjet tabelisse "Person" vale vanusega.
+	
 --?
 select * from Person
 go
