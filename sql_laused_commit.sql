37ae8314373ffc906077c45e8660f1ab159f69e8
Opilane TTHK <opilane@tthk.ee>
2024-04-30 10:12:05 +0300

    võtab sisendparameetri  @Id

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -339,7 +339,7 @@ declare @FirstName nvarchar(50)
 execute spGetNameById1 6, @FirstName output
 print 'Name of the employee = ' + @FirstName
 
---võtab sisendparameetri "@Id" 
+--võtab sisendparameetri  "@Id" 
 create proc spGetNameById2
 @Id int
 as begin
