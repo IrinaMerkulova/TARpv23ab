93f1d193e276d58d272ab81258c7194d8663fac1
Marija Gorbunova <mariarapirovna@gmail.com>
2024-04-30 10:09:56 +0300

    vanuse sumeerimine tabelis Person

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -136,7 +136,7 @@ select top 50 percent * from Person
 select * from Person order by cast(Age as int)
 select * from Person order by Age
 
---?
+-- vanuse sumeerimine 
 select sum(cast(Age as int)) from Person
 
 --- kuvab kõige nooremat isikut
