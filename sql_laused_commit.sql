6cedf6cbf03d3e8f8977818de9ff866dde76ee55
FantomTchi7 <fantomcorp@outlook.com>
2024-04-30 09:12:38 +0300

    -- näitab, kelle on nimis ees WAS märke polnud

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -121,7 +121,7 @@ select * from Person where Email not like '%@%'
 -- ainult üks täht
 select * from Person where Email like '_@_.com'
 
---?
+-- näitab, kelle on nimis ees WAS märke polnud
 select * from Person where Name like '[^WAS]%'
 --- ?
 select * from Person where (City = 'Gotham' or City = 'New York')
