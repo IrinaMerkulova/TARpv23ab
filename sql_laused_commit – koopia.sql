01c87650da1c1006a8525352dde6bbae72b5b4b1
Bogdan Sergachev <kseeyz@gmail.com>
2024-04-30 09:12:06 +0300

    lisab tabelisse Isik piirangu CHECK, mis kontrollib, et väärtus veerus Vanus oleks suurem kui 0 ja väiksem kui 150.

--- sql_laused_commit – koopia.sql
+++ sql_laused_commit – koopia.sql
@@ -78,7 +78,7 @@ update Person
 set Age = 149
 where Id = 8
 
--- 
+-- добавляет в таблицу "Person" ограничение "CHECK", которое проверяет, чтобы значение в столбце Age было больше 0 и меньше 150.
 
 alter table Person
 add constraint CK_Person_Age check (Age > 0 and Age < 150)
