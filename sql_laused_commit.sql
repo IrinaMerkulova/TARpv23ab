66f2ca177f9235ec8e960363d31c24584d13f061
Daria Halchenko <daragalcenko3@gmail.com>
2024-04-30 10:19:15 +0300

    Protseduuri loomine Id-i töötajate nime saamiseks

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -340,7 +340,7 @@ declare @TotalEmployees int
 execute spTotalCount2 @TotalEmployees output
 select @TotalEmployees
 
---- ?
+--- Protseduuri loomine Id-i töötajate nime saamiseks
 create proc spGetNameById1
 @Id int,
 @FirstName nvarchar(50) output
