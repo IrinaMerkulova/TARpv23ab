708ce71113bc4f804839e978ee756cfc5df04736
Martin Kemppi <dusperin1234@gmail.com>
2024-04-30 10:14:16 +0300

    töötaja nime

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -340,7 +340,7 @@ declare @FirstName nvarchar(50)
 execute spGetNameById1 6, @FirstName output
 print 'Name of the employee = ' + @FirstName
 
---?
+-- tagastab määratud identifikaatoriga (@Id) töötaja nime.
 create proc spGetNameById2
 @Id int
 as begin
