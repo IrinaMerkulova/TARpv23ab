3d0b1bcb28be685dea3f856820b97c2bdc1579cc
Opilane TTHK <opilane@tthk.ee>
2024-04-30 10:08:03 +0300

    võtab sisendparameetri @Id tüüpi INT

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -326,7 +326,7 @@ declare @TotalEmployees int
 execute spTotalCount2 @TotalEmployees output
 select @TotalEmployees
 
---- ?
+--- võtab sisendparameetri "@Id" tüüpi INT
 create proc spGetNameById1
 @Id int,
 @FirstName nvarchar(50) output
