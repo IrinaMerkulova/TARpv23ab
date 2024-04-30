772d9c3557efaecc6dba1256bc08505e068d01f8
Lev Jegorov <Levjegorov22@gmail.com>
2024-04-30 09:27:53 +0300

    CHECK piirangu lisamine ja lisab andmed

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -78,12 +78,12 @@ update Person
 set Age = 149
 where Id = 8
 
---?
-alter table Person
-add constraint CK_Person_Age check (Age > 0 and Age < 150)
+--CHECK piirangu lisamine ja lisab andmed
+	alter table Person
+	add constraint CK_Person_Age check (Age > 0 and Age < 150)
 
-insert into Person (Id, Name, Email, GenderId, Age)
-values (9, 'Test', 'Test', 2, 160)
+	insert into Person (Id, Name, Email, GenderId, Age)
+	values (9, 'Test', 'Test', 2, 160)
 
 --?
 select * from Person
