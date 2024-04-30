18f5026b9d0d6efbc7983f7f26938451aea9e3c0
Gleb Sotsjov <opilane@tthk.ee>
2024-04-30 09:56:33 +0300

    tagastab töötaja nime antud identifikaatori järgi.

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -335,7 +335,7 @@ declare @TotalEmployees int
 execute spTotalCount2 @TotalEmployees output
 select @TotalEmployees
 
---- ?
+--- tagastab töötaja nime antud identifikaatori järgi.
 create proc spGetNameById1
 @Id int,
 @FirstName nvarchar(50) output
