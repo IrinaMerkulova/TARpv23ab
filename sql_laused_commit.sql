3b7d018bd091ddd59fdfec26fb8b654df6366203
Jelizaveta Ostapjuk <jelizaveta.ostapjuk.work@gmail.com>
2024-04-30 09:26:25 +0300

    vanuste summa

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -140,7 +140,7 @@ select top 50 percent * from Person
 select * from Person order by cast(Age as int)
 select * from Person order by Age
 
---?
+-- vanuste summa 
 select sum(cast(Age as int)) from Person
 
 --- kuvab kõige nooremat isikut
