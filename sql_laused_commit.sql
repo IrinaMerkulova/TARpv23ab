232e7bdb7f189963296d547c1d02ec6941090720
DavidMyrseth <153905309+DavidMyrseth@users.noreply.github.com>
2024-05-08 16:37:48 +0300

    -- Kontrollib tabeli Isik veergu Vanus.

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -103,7 +103,7 @@ where Id = 8
 --?
 alter table Person
 add constraint CK_Person_Age check (Age > 0 and Age < 150)
--- Lisab kontrolli veeru "Age" jaoks tabelisse "Person".
+-- Kontrollib tabeli Isik veergu Vanus.
 	
 insert into Person (Id, Name, Email, GenderId, Age)
 values (9, 'Test', 'Test', 2, 160)
