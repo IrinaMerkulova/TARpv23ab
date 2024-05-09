51dd31f4bd80cea70f0fe8935f049dc0ecef7cd2
DavidMyrseth <153905309+DavidMyrseth@users.noreply.github.com>
2024-05-09 19:08:14 +0300

    -- päring lisab tabelisse "Töötajad" uue veeru "Linn".

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -272,7 +272,7 @@ select min(cast(Salary as int)) from Employees
 
 alter table Employees
 add City nvarchar(25)
--- päring lisab uue veeru "City" tabelisse "Employees".
+-- päring lisab tabelisse "Töötajad" uue veeru "Linn".
 
 alter table Employees
 add DepartmentId
