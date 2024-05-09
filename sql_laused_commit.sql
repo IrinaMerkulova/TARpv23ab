b327ecb8d74b9bca753c425470fe113673de4e7b
DavidMyrseth <153905309+DavidMyrseth@users.noreply.github.com>
2024-05-09 19:40:08 +0300

    -- Taotlege töötaja nime saamist tema ID-ga ja kuvage see.

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -412,6 +412,6 @@ end
 declare @EmployeeName nvarchar(50)
 exec @EmployeeName = spGetNameById2 1
 print 'Name of the employee = ' + @EmployeeName
--- Saada töötaja nimi tema ID järgi ja kuvada see
+-- Taotlege töötaja nime saamist tema ID-ga ja kuvage see.
 	
 select * from Employees
