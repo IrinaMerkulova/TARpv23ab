1a6bc5e30e45b2f0232b04bb69a4a199e047f527
David-Lennuk <153904926+David-Lennuk@users.noreply.github.com>
2024-05-13 12:13:21 +0300

    Update asdad.sql

--- asdad.sql
+++ asdad.sql
@@ -69,28 +69,39 @@ select * from Person where Email not like '%@%'
 -- ainult üks täht
 select * from Person where Email like '_@_.com'
 
+	
+-- шоу, перед именем которых не было символов WAS
 -- näitab, kelle on nimis ees WAS märke polnud
 select * from Person where Name like '[^WAS]%'
 -- näitab inimest, kes elab Gothamis või New Yorkis, ja on vanem kui 41
 select * from Person where (City = 'Gotham' or City = 'New York')
 and Age >= 40
 
+	
+--- занимает первые три строки
 ---võtab kolm esimest rida
 select top 3 * from Person
 
+	
+--- первые три, но порядок таблицы: Возраст, а затем Имя.
 --- kolm esimest, aga tabeli järjestus on Age ja siis Name
 select * from Person
 select top 3 Age, Name from Person
 
+	
+--- показывает первые 50% таблицы
 --- näitab esimesed 50% tabelis
 select top 50 percent * from Person
 -- näitab inimesi vanuse järgi sorteeritud
 select * from Person order by cast(Age as int)
 select * from Person order by Age
 
+	
+-- показывает общий возраст людей
 -- näitab inmesi summarne vanus
 select sum(cast(Age as int)) from Person
 
+--- отображает самого молодого человека
 --- kuvab kõige nooremat isikut
 select min(cast(Age as int)) from Person
 --- kõige vanem isik
@@ -103,9 +114,13 @@ select City, sum(cast(Age as int)) as TotalAge from Person group by City
 
 
 --- tund 3
+	
+-- Показывает имена сотрудников и идентификаторы отделов
 -- Näitab töötajate nimid ja osakonna Id'ed
 select distinct Name, DepartmentId from Employees
 
+	
+-- Показывает общую зарплату сотрудников
 -- Näitab summarne töötajate palk
 select sum(cast(Salary as int)) from Employees
 -- Näitab minimaalne töötajate palk
@@ -121,6 +136,7 @@ add DepartmentId
 int null
 
 
+--создание столбца
 --veerge loomine
 alter table Employees
 add MiddleName nvarchar(30)
@@ -152,6 +168,7 @@ update Employees set FirstName = NULL, MiddleName = NULL, LastName = 'Crowe'
 where Id = 10
 
 
+--- из каждой строки берет заполненную ячейку в первом столбце и отображает только ее
 --- igast reast võtab esimeses veerus täidetud lahtri ja kuvab ainult seda
 select Id, coalesce(FirstName, MiddleName, LastName) as Name
 from Employees
@@ -163,6 +180,7 @@ select * from Department
 
 
 
+--- мы создаем хранимую процедуру, которая отображает представление
 --- loome stored procedure, mis kuvab vaate
 create procedure spGetEmployees
 as begin
@@ -173,6 +191,8 @@ spGetEmployees
 exec spGetEmployees
 execute spGetEmployees
 
+	
+--- создаём хранимую процедуру, получаем сотрудников по полу и отделу
 --- loome stored procedure, Hankige töötajaid soo ja osakonna järgi
 create proc spGetEmployeesByGenderAndDepartment
 @Gender nvarchar(20),
@@ -182,6 +202,8 @@ as begin
 	and DepartmentId = @DepartmentId
 end
 
+	
+--- все сотрудники мужского пола в первом отделе
 --- kõik esimeses osakonnas meessoost töötavad isikud
 exec spGetEmployeesByGenderAndDepartment 'Male', 1
 
@@ -189,6 +211,7 @@ exec spGetEmployeesByGenderAndDepartment @DepartmentId =  1, @Gender = 'Male'
 
 
 
+-- создаем хранимую процедуру, получаем количество сотрудников по полу
 -- loome stored procedure, Hankige töötajate arv soo järgi
 create proc spGetEmployeeCountByGender
 @Gender nvarchar(20),
@@ -197,6 +220,7 @@ as begin
 	select @EmployeeCount = count(Id) from Employees where Gender = @Gender
 end
 
+-- сообщает, сколько лиц мужского пола, и отображает соответствующую строку	
 -- annab teada, palju on meessoost isikuid ning kuvab vastava stringi
 declare @TotalCount int
 exec spGetEmployeeCountByGender 'Female', @TotalCount out
@@ -206,22 +230,30 @@ else
 	print '@TotalCount is not null'
 print @TotalCount
 
+-- указывает, сколько лиц мужского пола
 -- annab teada, palju on meessoost isikuid
 declare @TotalCount int
 exec spGetEmployeeCountByGender @EmployeeCount = @TotalCount out, @Gender = 'Male'
 print @TotalCount
 
+	
+--- создать хранимую процедуру, получить количество сотрудников
 --- create stored procedure, Hankige töötajate arv
 create proc spTotalCount2
 @TotalCount int output
 as begin
 	select @TotalCount = count(Id) from Employees
 end
+
+	
+--- начнем сп
 --- käivitame sp
 declare @TotalEmployees int
 execute spTotalCount2 @TotalEmployees output
 select @TotalEmployees
 
+	
+--- создать хранимую процедуру, получить имя по идентификатору
 --- create stored procedure, saab nime ID järgi
 create proc spGetNameById1
 @Id int,
@@ -230,11 +262,15 @@ as begin
 	select @FirstName = FirstName from employees where Id = @Id
 end
 
+	
+-- выполняет процедуру, получает имя по ID
 -- teostab protseduuri, saab nime ID järgi
 declare @FirstName nvarchar(50)
 execute spGetNameById1 6, @FirstName output
 print 'Name of the employee = ' + @FirstName
 
+	
+-- создаем хранимую процедуру, получаем имя по ID 2
 -- create stored procedure, saab nime ID järgi 2
 create proc spGetNameById2
 @Id int
@@ -242,6 +278,8 @@ as begin
 	return (select FirstName from Employees where Id = @Id)
 end
 
+	
+-- выполняет процедуру, получает имя по ID 2
 -- teostab protseduuri, saab nime ID järgi 2
 declare @EmployeeName nvarchar(50)
 exec @EmployeeName = spGetNameById2 1
