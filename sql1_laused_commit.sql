d433f56d2e53a7bab0cb00ce579d659a1f1914eb
David-Lennuk <153904926+David-Lennuk@users.noreply.github.com>
2024-05-13 12:39:08 +0300

    tabelite loomine

---
+++ sql1_laused_commit.sql
@@ -0,0 +1,48 @@
+-- db loomine
+create database Tarpv23
+
+-- db kustutamine
+DRop DataBASE Tarpv23
+
+-- gender ja person tabel loomine
+create table Gender
+(
+Id int NOT NULL primary key,
+Gender nvarchar(10) not null
+)
+-----------------------------------------------------------
+                      teine ​​tabel
+-----------------------------------------------------------	
+create table Person
+(
+Id int not null primary key,
+Name nvarchar(25),
+Email nvarchar(30),
+GenderId int
+)
+
+--- foreign key loomine
+alter table Person add constraint tblPerson_GenderId_FK
+foreign key (GenderId) references Gender(Id)
+-----------------------------------------------------------
+	               kolmas tabel
+-----------------------------------------------------------
+--- loome uued tabelid
+create table Department
+(
+Id int primary key,
+DepartmentName nvarchar(50),
+Location nvarchar(50),
+DepartmentHead nvarchar(50)
+)
+-----------------------------------------------------------
+	               neljas tabel
+-----------------------------------------------------------	
+create table Employees
+(
+Id int primary key,
+Name nvarchar(50),
+Gender nvarchar(10),
+Salary nvarchar(50),
+DepartmentId int
+)
