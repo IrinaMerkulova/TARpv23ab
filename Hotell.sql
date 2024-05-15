78137e880a8232c308176b029fa352d3ef95d5c0
Timur-Basirov <153905042+Timur-Basirov@users.noreply.github.com>
2024-05-15 15:18:03 +0300

    guest tabeli loomine

--- Hotell.sql
+++ Hotell.sql
@@ -22,3 +22,13 @@ foreign key (room_type_id) references room_type(id));
 
 INSERT INTO room(number, name, status, smoke, room_type_id)
 VALUES ('A09', 'LUX', 'open', 1, 1);
+
+--guest tabeli loomine
+CREATE TABLE guest(
+id int Primary key identity (1,1),
+first_name varchar (80),
+last_name varchar (80),
+member_since date);
+
+INSERT INTO guest(first_name, last_name, member_since)
+VALUES ('Timur', 'Basirov', '2024-02-19');
