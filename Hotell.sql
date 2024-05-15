e88c8bf8ce846b22b29b99cabae81d9cf25a4fdf
Timur-Basirov <153905042+Timur-Basirov@users.noreply.github.com>
2024-05-15 15:14:51 +0300

    room tabeli loomine

--- Hotell.sql
+++ Hotell.sql
@@ -9,3 +9,16 @@ max_capacity int);
 
 INSERT INTO room_type(description, max_capacity)
 VALUES ('Common', 1);
+
+--room tabeli loomine 
+CREATE TABLE room(
+id int Primary key identity (1,1),
+number varchar (10),
+name varchar (40),
+status varchar (10),
+smoke bit,
+room_type_id int,
+foreign key (room_type_id) references room_type(id));
+
+INSERT INTO room(number, name, status, smoke, room_type_id)
+VALUES ('A09', 'LUX', 'open', 1, 1);
