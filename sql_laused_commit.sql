c9ff2ac2d9d65632038ca4da7a467cd3c546bf6f
kirillsats <153904960+kirillsats@users.noreply.github.com>
2024-05-20 18:01:13 +0300

    Näitab inmesi summarne vanus sql_laused_commit.sql

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -140,7 +140,7 @@ select top 50 percent * from Person
 select * from Person order by cast(Age as int)
 select * from Person order by Age
 
---?
+--?Näitab inmesi summarne vanus
 select sum(cast(Age as int)) from Person
 
 --- 
