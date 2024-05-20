863b116de86d39e1d5334fba48df57a95be97d22
kirillsats <153904960+kirillsats@users.noreply.github.com>
2024-05-20 17:49:32 +0300

    näitab inimesi, kes on vanuses 30-50 aastat sql_laused_commit.sql

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -107,7 +107,7 @@ select *from Person where Age = 100 or
 Age = 50 or Age = 20
 select * from Person where Age in (100, 50, 20)
 
---- 
+--- näitab inimesi, kes on vanuses 30-50 aastat
 select * from Person where Age between 30 and 50
 
 --- 
