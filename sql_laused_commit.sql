163c7425ad9e4991dcb53887460ebb50b93284df
kirillsats <153904960+kirillsats@users.noreply.github.com>
2024-05-20 17:47:46 +0300

    näitab teatud vanusega inimesi sql_laused_commit.sql

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -102,7 +102,7 @@ select * from Person where City = 'Gotham'
 select * from Person where City <> 'Gotham'
 select * from Person where City != 'Gotham'
 
--- 
+-- näitab teatud vanusega inimesi
 select *from Person where Age = 100 or 
 Age = 50 or Age = 20
 select * from Person where Age in (100, 50, 20)
