68b0091b80c424827e0eb98416ada93e8917ec53
GlebSotsjov <153904865+GlebSotsjov@users.noreply.github.com>
2024-06-02 23:23:21 +0300

    tabel ruum_type on lisatud

--- Hotel.sql
+++ Hotel.sql
@@ -1,3 +1,12 @@
 -- Salvestame TARpv23ab kausta sisse CTRL + S
 create database hotellSots;
 use hotellSots;
+
+-- tabel room_type
+create table room_type(
+id int primary key identity(1,1),
+description varchar(80),
+max_capacity int);
+select * from room_type;
+insert into room_type(description, max_capacity)
+values('SUPER LUX',1)
