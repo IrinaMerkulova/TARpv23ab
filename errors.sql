87c63a9be1ebb5587b28ebcd3fe5673a1a387a4c
BogdanViblyy <153904857+BogdanViblyy@users.noreply.github.com>
2024-06-11 22:36:19 +0300

    Tüübi sobimatus (Type Mismatch)
    Üritatakse lisada string numbrilisele väljale

--- errors.sql
+++ errors.sql
@@ -6,3 +6,7 @@ CREATE TABLE Tootajad (
     ID INT,
     Nimi VARCHAR(100
 )
+
+-- Tüübi sobimatus
+-- Yritatakse lisada string numbrilisele väljale
+INSERT INTO Tootajad (ID, Nimi) VALUES ('üks', 'Bog');
