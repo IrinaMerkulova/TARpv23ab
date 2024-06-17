e3eb715d81e58ff2f5411f1dcaac7b9eb5cc575e
Aleksandr Nadvornõi <120181396+tiraforgivewin@users.noreply.github.com>
2024-06-17 12:48:36 +0300

    Update sql_laused_commit.sql
    andis mitmete päringute ja käskude kirjelduse.

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -223,7 +223,7 @@ add DepartmentId
 int null
 
 
--- ?
+--tabeli muudatused
 alter table Employees
 add MiddleName nvarchar(30)
 
@@ -252,12 +252,13 @@ update Employees set FirstName = NULL, MiddleName = NULL, LastName = 'Crowe'
 where Id = 10
 
 
---- ?
+--- igast reast võtab esimeses veerus täidetud lahtri ja kuvab ainult seda
 select Id, coalesce(FirstName, MiddleName, LastName) as Name
 from Employees
 
 select * from Employees
 select * from Department
+
 
 
 
