7520c1186f5c69b3c2712b7f33c0a44f484fc6a7
Aleksandr Nadvornõi <120181396+tiraforgivewin@users.noreply.github.com>
2024-06-17 12:43:24 +0300

    Update sql_laused_commit.sql
    andis mitmete päringute ja käskude kirjelduse.

--- sql_laused_commit.sql
+++ sql_laused_commit.sql
@@ -105,37 +105,38 @@ select * from Person where City != 'Gotham'
 select *from Person where Age = 100 or 
 Age = 50 or Age = 20
 select * from Person where Age in (100, 50, 20)
---- ?
+--- näitab teatud vanusevahemikus olevaid inimesi
 select * from Person where Age between 30 and 50
 
---- ?
+--- wildcard e näitab kõik g-tähega linnad
 select * from Person where City like 'n%'
 select * from Person where Email like '%@%'
 
---?
+-- näitab kõiki, kellel ei ole @-märki emailis
 select * from Person where Email not like '%@%'
 
---- ?
+--- näitab, kelle on emailis ees ja peale @-märki
+-- ainult üks täht
 select * from Person where Email like '_@_.com'
---? 
+
+--otsib, kus veeru "Email" väärtus ei alga tähtedega W,A või S
 select * from Person where Name like '[^WAS]%'
---?
+---näitab kõiki kirjeid tabelist, kus linn on 'Gotham', 'New York' ja 40 aastane või vanem
 select * from Person where (City = 'Gotham' or City = 'New York')
 and Age >= 40
---?
 
+---võtab kolm esimest rida
 select top 3 * from Person
 
---?
+--- kolm esimest, aga tabeli järjestus on Age ja siis Name
 select * from Person
 select top 3 Age, Name from Person
 
---- ?
+--- näitab esimesed 50% tabelis
 select top 50 percent * from Person
---?
+---- näitab inimesi vanuse järgi sorteeritud
 select * from Person order by cast(Age as int)
 select * from Person order by Age
-
 --?
 select sum(cast(Age as int)) from Person
 
