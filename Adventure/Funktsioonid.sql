e58ede073d9cf2add556062ea6e95423258827f3
RipMyMartin <161806791+RipMyMartin@users.noreply.github.com>
2024-10-01 08:56:38 +0300

    33 Funktsioonid

--- Adventure/Funktsioonid.sql
+++ Adventure/Funktsioonid.sql
@@ -1,3 +1,4 @@
+--32 Funktsioonid
 --Tabelisiseväärtusega funktsioon e Inline Table Valued function (ILTVF) koodinäide:
 create function fn_ILTVF_GetEmployees()
 
@@ -8,20 +9,49 @@ Return (Select EmployeeKey ,FirstName,Cast(BirthDate as Date) as DOB
 
 --Mitme avaldisega tabeliväärtusega funktsioonid e multi-statement table valued function (MSTVF):
 create function fn_MSTVF_GetEmployees()
-
 Returns @Table table (EmployeeKey int, FirstName nvarchar(20),DOB Date)
 as
 begin
 insert into @Table
 Select EmployeeKey,FirstName,CAST(BirthDate as Date)
 From DimEmployee
-
 return
 End
+	
 --Kui nüüd soovid mõlemat funktsiooni esile kutsuda, siis kasutad koodi:
 Select * from fn_ILTVF_GetEmployees()
 Select * from fn_MSTVF_GetEmployees()
+	
 --Uuendame allasuvat tabelit ja kasutame selleks ILTVF funktsiooni
 Update fn_ILTVF_GetEmployees() set FirstName='Sam1' where EmployeeKey=1;
 Select * from fn_ILTVF_GetEmployees()
 
+--33 Funktsioonid
+--Skaleeritav funktsioon ilma krüpteerimata
+Create Function fn_GetEmpoyeeNameById(@Id int)
+Returns nvarchar(20)
+as
+Begin
+return (Select FirstName From DimEmployee Where EmployeeKey =@Id)
+END
+
+--Funktsiooni sisu vaatamise koodinäide
+sp_helptext fn_GetEmployeeNameById
+
+--Nüüd muudame funktsiooni ja krüpteerime selle ära:
+Alter Function fn_GetEmployeeNameById(@Id int)
+returns nvarchar(20)
+With Encryption
+as
+Begin
+return (Select FirstName from DimEmployee where EmployeeKey = @Id)
+END
+
+--Nüüd muuda funktsiooni ja kasuta käsklust WITH SCHEMABINDING valikut
+Alter function fn_GetEmpoyeeNameById(@Id int)
+Returns nvarchar(20)
+With SchemaBinding
+as
+begin
+Return (Select FirstName from DimEmployee where EmployeeKey = @Id)
+END
