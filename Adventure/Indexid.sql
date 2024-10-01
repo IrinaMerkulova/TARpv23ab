6d67fadaf08b1a02d3875260e132b6dc1e7ef62e
RipMyMartin <161806791+RipMyMartin@users.noreply.github.com>
2024-10-01 09:23:24 +0300

    36. Klastreeritud ja mitte-klastreeritud indeksid

--- Adventure/Indexid.sql
+++ Adventure/Indexid.sql
@@ -11,3 +11,25 @@ EXEC sp_help 'DimEmployee';
 --Kui soovid kustutada indeksit
 drop Index DimEmployee.IX_tblEmployee_Salary
 
+--36. Klastreeritud ja mitte-klastreeritud indeksid
+
+--mis näitab uniklaaset klastreeritud indeksi loomist Id veerus.
+execute sp_helpindex DimEmployee
+
+--Vaatame DimEmployee
+Select * from DimEmployee
+
+--Selle tulemusel SQL server ei luba luua rohkem, kui ühte klastreeritud indeksit tabeli kohta. Järgnev skript annab veateate
+Create Clustered Index IX_DimEmpoyee_Name
+on DimEmployee(FirstName)
+
+--Nüüd loome klastreeritud indeksi kahe veeruga. Selleks peame enne kustutama praeguse klastreeritud indeksi Id veerus:
+Drop index DimEmployee
+
+--Nüüd käivita järgnev kood uue klastreeritud ühendindeksi loomiseks Gender ja Salary veeru põhjal
+CREATE CLUSTERED INDEX IX_DimEmployee_Gender_Salary
+ON DimEmployee(Gender DESC, BaseRate ASC);
+
+--Järgnev kood loob SQL-s mitte-klastreeritud indeksi Name veeru järgi DimEmployee tabelis
+Create NonClustered Index IX_DimEmployee_Name
+On DimEmployee (FirstName)
