24e4cf05ed26f7bae324132017724f6d4a6016fe
RipMyMartin <161806791+RipMyMartin@users.noreply.github.com>
2024-10-01 09:09:38 +0300

    Index

--- Adventure/Indexid.sql
+++ Adventure/Indexid.sql
@@ -1,1 +1,13 @@
+--Indexid.sql
+--Soovime leida kõik töölised, kelle palk on vahemikus 5000 kuni 7000
+Create index IX_tblEmployee_Salary
+on DimEmployee (BaseRate ASC)
+
+Select * from DimEmployee where BaseRate > 5000 and BaseRate < 7000
+
+--Kui soovid vaadata Indeksit
+EXEC sp_help 'DimEmployee'; 
+
+--Kui soovid kustutada indeksit
+drop Index DimEmployee.IX_tblEmployee_Salary
 
