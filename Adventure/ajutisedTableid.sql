47e911c9ba68f54ea5edf913a65cdda49e6e729d
MarijaGorbunova0 <153904965+MarijaGorbunova0@users.noreply.github.com>
2024-10-01 10:31:29 +0300

    Update ajutisedTableid.sql

---
+++ Adventure/ajutisedTableid.sql
@@ -0,0 +1,26 @@
+-- ajutisedTableid 34
+create table #PersonDetails(
+Id int,
+Name nvarchar(20))
+
+Insert into #PersonDetails Values(1,'Mike')
+Insert into #PersonDetails Values(2,'John')
+Insert into #PersonDetails Values(3,'Todd')
+select * from  #PersonDetails
+Select * from #PersonDetails
+
+Select name from tempdb..sysobjects
+where name like '#PersonDetails%'
+
+Create Procedure spCreateLocalTempTable
+as
+Begin
+Create Table #PersonDetails(Id int, Name nvarchar(20))
+Insert into #PersonDetails Values(1,'Mike')
+Insert into #PersonDetails Values(2,'John')
+Insert into #PersonDetails Values(3,'Todd')
+Select * from #PersonDetails
+End
+EXEC spCreateLocalTempTable
+
+CREATE TABLE ##EmployeeDetails (Id int, Name nvarchar(20));
